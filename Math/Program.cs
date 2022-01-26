5fdd5d9d0ec9260f7d9603adccad4a7d0a2a8197
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-26 13:15:12 -0700

    finished

--- Math/Program.cs
+++ Math/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Math
+namespace Circle
 {
-   class Program
-   {
+    class circle { 
+      
+
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+
+         float MyPi = 3.14159f;
+         int r;
+         
+         Console.WriteLine("Enter Radius"); //prompt user
+         // read diameter from user
+         r = int.Parse(Console.ReadLine());
+
+         Console.WriteLine($"Diameter = {2 * r}");
+         Console.WriteLine($"Circumference = {2 * MyPi * r}");
+         Console.WriteLine($"area = {MyPi * r * r}");
       }
    }
 }
+
